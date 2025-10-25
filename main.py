c59d203320401d4fbf45c82154a0dd06115e9cb5
Binaya764 <bigyanlimbu179@gmail.com>
2025-10-25 09:13:28 +0545

    pytube

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello world")
+from pytube import YouTube
+import tkinter as tk
+from tkinter import filedialog
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        streams = yt.streams.filter(progressive = True, file_extension = "mp4")
+        highest_res_stream = streams.get_highest_resolution()
+        highest_res_stream.download(output_path = save_path)
+    except Exception as e:
+        print(e)
+    
+url =   "https://www.youtube.com/watch?v=9DSBkbCcfFI"
+
+
+save_path = r"C:\Users\Asus\Downloads\Video"
+download_video(url,save_path)
+
+    
+    
